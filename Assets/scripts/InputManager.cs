1fe88e9195ab65cf076fcc5c0d2679ded5decc6b
Alic Szecsei <aszecsei@gmail.com>
2019-02-19 10:53:10 -0600

    Add input management

---
+++ Assets/scripts/InputManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputManager : MonoBehaviour
+{
+	public static InputManager Instance;
+	
+	public abstract Vector3 GetHandPosition();
+}
---
+++ Assets/scripts/KinectInputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinectInputManager : InputManager {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override Vector3 GetHandPosition()
+	{
+		return Vector3.zero;
+	}
+}
