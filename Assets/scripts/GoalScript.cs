7f9e62e20e75e50df04aec3aba6bef13e07a7279
Heather K <heatherkemp7@yahoo.com>
2019-03-30 21:56:30 -0500

    Add goals and continue transfering logic

---
+++ Assets/scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GoalScript : MonoBehaviour {
+
+    public UnityEvent onPlayerGoalScore;
+    public UnityEvent onOpponentGoalScore;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            if (gameObject.CompareTag("SouthGoal"))
+            {
+                onOpponentGoalScore.Invoke();
+            }
+            if (gameObject.CompareTag("NorthGoal") && other.gameObject.GetComponent<BallScript>().ballHitOnce)
+            {
+                onPlayerGoalScore.Invoke();
+            }
+        }
+    }
+}
