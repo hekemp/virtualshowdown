bf1ac189a9c4d1027d2ab256d2fcad30c0b25778
Heather K <heatherkemp7@yahoo.com>
2019-04-25 02:33:01 +0100

    Finish updating main menu

--- Assets/scripts/MainMenuSetup.cs
+++ Assets/scripts/MainMenuSetup.cs
@@ -12,6 +12,19 @@ public class MainMenuSetup : MonoBehaviour
 	public Button ShowdownBtn;
 	public Button PreferencesBtn;
 
+    public AudioClip welcomeSFXClip;
+    public AudioClip welcomeClip;
+
+    public AudioClip readShowdownOptionClip;
+    public AudioClip readDrillOptionClip;
+    public AudioClip readPreferencesOptionClip;
+    public AudioClip readExplainShowdownOptionClip;
+    public AudioClip readExplainDrillOptionClip;
+    public AudioClip readRepeatOptionClip;
+
+    public AudioClip explainShowdownClip;
+    public AudioClip explainDrillClip;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -19,12 +32,43 @@ public class MainMenuSetup : MonoBehaviour
 		ShowdownBtn.onClick.AddListener(StartShowdown);
 		PreferencesBtn.onClick.AddListener(StartPreferencesMenu);
 
-        // TODO: play welcome music
-        // TODO: play welcome audio
+        StartCoroutine(playIntroductionNarration());
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void playOptionsAgain()
+    {
+        StartCoroutine(readAllOptions());
+    }
+
+    IEnumerator readAllOptions()
+    {
+        AudioManager.Instance.PlayNarration(readShowdownOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        AudioManager.Instance.PlayNarration(readDrillOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        AudioManager.Instance.PlayNarration(readPreferencesOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        AudioManager.Instance.PlayNarration(readExplainShowdownOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        AudioManager.Instance.PlayNarration(readExplainDrillOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        AudioManager.Instance.PlayNarration(readRepeatOptionClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+        yield return null;
+    }
+
+    IEnumerator playIntroductionNarration()
+    {
+        AudioManager.Instance.PlaySfx(welcomeSFXClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+        AudioManager.Instance.PlayNarrationImmediate(welcomeClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+        yield return new WaitForSeconds(Mathf.Max(welcomeSFXClip.length, welcomeClip.length));
+
+        StartCoroutine(readAllOptions());
+
+}
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -45,5 +89,18 @@ public class MainMenuSetup : MonoBehaviour
 		Debug.Log("Going to preference mode");
 		SceneManager.LoadScene("Menu_Setup");
 	}
+
+    public void explainShowdown()
+    {
+
+        AudioManager.Instance.PlayNarrationImmediate(explainShowdownClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+    }
+
+    public void explainDrill()
+    {
+        AudioManager.Instance.PlayNarrationImmediate(explainDrillClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+
+    }
 
 }
--- Assets/scripts/MenuSetupSection.cs
+++ Assets/scripts/MenuSetupSection.cs
@@ -176,6 +176,7 @@ public class MenuSetupSection : MonoBehaviour
 		}
 	}
 
+
 	void Finish()
 	{
 //		foreach (var btn in Buttons)
