bd231e6eb2268fc7b9c1c618dab1233c824b246c
Heather Kemp <heather-kemp@uiowa.edu>
2019-05-01 13:42:02 -0500

    Add accessible narration class

---
+++ Assets/scripts/AccessibleUIElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+/// <summary>
+/// Class that is attached to all UI that should be accessible
+/// </summary>
+public class AccessibleUIElement : MonoBehaviour, IPointerEnterHandler, ISelectHandler {
+
+	public AudioClip hoverAudioClip;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		AudioManager.Instance.PlayNarrationImmediate(hoverAudioClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+	}
+	public void OnSelect(BaseEventData eventData)
+	{
+		AudioManager.Instance.PlayNarrationImmediate(hoverAudioClip, AudioManager.Instance.locationSettings[AudioManager.AudioLocation.Default]);
+	
+	}
+		
+}
