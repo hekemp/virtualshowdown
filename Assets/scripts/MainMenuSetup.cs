272318b25fa467e72eca2aa45ae96403755d263f
Heather Kemp <heather-kemp@uiowa.edu>
2019-05-05 22:29:04 -0500

    revert quit back to normal

--- Assets/scripts/MainMenuSetup.cs
+++ Assets/scripts/MainMenuSetup.cs
@@ -133,14 +133,8 @@ public class MainMenuSetup : MonoBehaviour
 	}
 
 	public void QuitGame(){
-		// save any game data here
-		#if UNITY_EDITOR
-		// Application.Quit() does not work in the editor so
-		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-		UnityEditor.EditorApplication.isPlaying = false;
-		#else
-		Application.Quit();
-		#endif
+        // TODO: quit game here
+        Application.Quit();
     }
 
 }
